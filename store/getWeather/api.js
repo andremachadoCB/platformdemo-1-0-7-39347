d99536e37433c6f274a4fff1b9df35ab5aaffc84
Crowdbotics <engineering@crowdbotics.com>
2023-03-28 21:37:19 +0000

    Update store.

--- store/getWeather/api.js
+++ store/getWeather/api.js
@@ -1,21 +1,11 @@
-import axios from "axios";
+import axios from "axios"
 const getWeather = axios.create({
   baseURL: "https://api.openweathermap.org",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function getweather_get_data_25_weather_read(payload) {
   return getWeather.get(`/data/2.5/weather`, {
-    params: {
-      q: payload.q,
-      appid: payload.appid
-    }
-  });
+    params: { q: payload.q, appid: payload.appid }
+  })
 }
-
-export const apiService = {
-  getweather_get_data_25_weather_read
-};
+export const apiService = { getweather_get_data_25_weather_read }
